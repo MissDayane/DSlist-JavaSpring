85bf3dffddd027209dd60931b06f6e6081534b30
MissDayane <day_pontess@hotmail.com>
2025-01-20 20:35:11 -0300

    Project Created

---
+++ src/main/java/com/devsuperir/DSlist/DSlistApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperir.DSlist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DSlistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DSlistApplication.class, args);
+	}
+
+}
